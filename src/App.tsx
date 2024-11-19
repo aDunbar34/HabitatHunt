efba0446bf8d17a9b031951b5769e53ccb670dd5
Adam <adam.dunbar.2021@uni.strath.ac.uk>
2024-11-19 18:36:21 +0000

    Added routing and 7yk

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,44 @@
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+
 import "./App.css";
 import HomePageTitle from "./components/HomePageTitle";
 import Button from "./components/startGameButton";
 
 function App() {
+  return (
+    <>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/game" element={<Game />}/>
+      </Routes>
+    </Router>
+    </>
+  )
+}
+
+function Home() {
+  const navigate = useNavigate();
+  
   return <>
-    <h1><HomePageTitle></HomePageTitle></h1>
+    <h1><HomePageTitle /></h1>
     <Button
       border="dashed"
       color="grey"
-      height = "10rem"
-      onClick={() => alert("Click")}
-      radius = "50%"
-      width = "10rem"
-      children = "Play!"
+      height="10rem"
+      onClick={() => navigate('/game')}
+      radius="50%"
+      width="10rem"
+      children="Play!"
     />
-
-  </>;
+  </>
 }
+
+function Game() {
+  return <>
+   <h1>Welcome to the Game Page!</h1>;
+  </>
+}
+
 
 export default App;

0ce659e3cfe685eec3d1b4268754a0e4d9e46abf
Adam <adam.dunbar.2021@uni.strath.ac.uk>
2024-11-19 17:39:27 +0000

    Fixed HomePageTitle and added a non-functional Start button

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
 import "./App.css";
 import HomePageTitle from "./components/HomePageTitle";
+import Button from "./components/startGameButton";
 
 function App() {
-  return <h1>temp heading</h1>;
+  return <>
+    <h1><HomePageTitle></HomePageTitle></h1>
+    <Button
+      border="dashed"
+      color="grey"
+      height = "10rem"
+      onClick={() => alert("Click")}
+      radius = "50%"
+      width = "10rem"
+      children = "Play!"
+    />
+
+  </>;
 }
 
 export default App;
--- src/components/HomePageTitle.tsx
+++ src/components/HomePageTitle.tsx
@@ -1,18 +1,5 @@
-// Handles all code to do with the Home Page heading
-import React from "react";
-
-// define the props for the component
-interface HomePageTitleProps {
-  title: string;
-  className: string;
+function HomePageTitle() {
+  return <h1>Habitat Hunt</h1>
 }
 
-// Functional Component
-const HomePageTitle: React.FC<HomePageTitleProps> = ({ title, className }) => {
-  return (
-    <div className={`home-page-title ${className || ""}`}>
-      <h1 className="home-page-title__main">{title}</h1>
-    </div>
-  );
-};
 export default HomePageTitle;

acef195f2fecdb679bc52af6ef8e5e5ebc256e22
Adam <Adam.Dunbar.2021@uni.strath.ac.uk>
2024-11-25 19:01:18 +0000

    edited map css to fit page better

--- src/components/map.tsx
+++ src/components/map.tsx
@@ -4,17 +4,14 @@ import { TileLayer } from "react-leaflet";
 function openMap() {
   return (
     <>
-      <h1>OpenStreetMap Game</h1>
       <MapContainer
         center={[51.505, -0.09]}
         zoom={3}
         zoomControl={false} // lock the map at the given zoom level
         scrollWheelZoom={false}
         style={{
-          height: "800px",
-          width: "1500px",
-          border: "5px solid black",
-          borderRadius: "10px",
+          height: "99vh",
+          width: "75vw",
           float: "right",
         }}
       >
